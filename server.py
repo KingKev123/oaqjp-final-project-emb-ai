6b5ca766a24728553c9651d951eb70b9273a14c8
Kingkev123 <bothakevin423@gmail.com>
2025-09-14 16:20:50 -0400

    Update port configuration for deployment

--- server.py
+++ server.py
@@ -53,6 +53,8 @@ def emotion_detector_app():
     return formatted_response
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+iif __name__ == "__main__":
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
     
